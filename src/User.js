ecc2430cc9ccbb30da4c31da5e407fef86c264db
MYEONGJIN-LEE <myeongjin3203@gmail.com>
2021-07-18 18:19:58 +0900

    api 연동하기

--- src/User.js
+++ src/User.js
@@ -1,20 +1,46 @@
 import axios from "axios";
-import React from "react";
-import useAsync from "./useAsync";
+import React, { useEffect } from "react";
+// import useAsync from "./useAsync";
+import { useAsync } from "react-async";
+import { getUser, useUsersDispatch, useUsersState } from "./UsersContext";
 
-const getUser = async (id) => {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/users/${id}`
-  );
-  return response.data;
-};
+/**
+ * react-async
+ * useAsync 를 사용할 때에는 프로미스를 반환하는 함수의 파라미터를 객체형태로 해주어야 합니다
+ */
+// const getUser = async (id) => {
+// const getUser = async ({ id }) => {
+//   const response = await axios.get(
+//     `https://jsonplaceholder.typicode.com/users/${id}`
+//   );
+//   return response.data;
+// };
 
 const User = (props) => {
   const { id } = props;
-  const [state] = useAsync(() => getUser(id), [id]);
-  const { loading, data: user, error } = state;
+  // const [state] = useAsync(() => getUser(id), [id]);
+  // const { loading, data: user, error } = state;
 
-  if (loading) return <div>로딩중..</div>;
+  //watch 값에 특정 값을 넣어주면 이 값이 바뀔 때마다 promiseFn 에 넣은 함수를 다시 호출
+  // const {
+  //   data: user,
+  //   error,
+  //   isLoading,
+  // } = useAsync({
+  //   promiseFn: getUser,
+  //   id,
+  //   watch: id,
+  // });
+
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+  useEffect(() => {
+    getUser(dispatch, id);
+  }, [dispatch, id]);
+
+  const {data:user, isLoading, error} = state.user;
+
+  if (isLoading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
   if (!user) return null;
 
--- src/Users.js
+++ src/Users.js
@@ -1,24 +1,39 @@
 import axios from "axios";
 import React, { useEffect, useReducer, useState } from "react";
-import useAsync from "./useAsync";
+import { useAsync } from "react-async";
+// import useAsync from "./useAsync";
 import User from "./User";
+import { getUsers, useUsersDispatch, useUsersState } from "./UsersContext";
 
-const getUsers = async () => {
-  const response = await axios.get(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  return response.data;
-};
+// const getUsers = async () => {
+//   const response = await axios.get(
+//     "https://jsonplaceholder.typicode.com/users"
+//   );
+//   return response.data;
+// };
 
 const Users = () => {
   const [userId, setUserId] = useState(null);
-  const [state, refetch] = useAsync(() => getUsers(), [], true);
+  // const [state, refetch] = useAsync(() => getUsers(), [], true);
+  // const { loading, data: users, error } = state;
 
-  const { loading, data: users, error } = state;
+  // const { data: users, error, isLoading, reload } = useAsync({
+  // const { data: users, error, isLoading, run } = useAsync({
+    // promiseFn : getUsers // 렌더링하는 시점 호출 (reload)
+    // deferFn : getUsers // 사용자의 특정 인터랙션에 따라 API 를 호출(reload 대신 run)
+  // });
 
-  if (loading) return <div>로당중...</div>;
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+
+  const {data: users, isLoading, error} = state.users;
+  const fetchData = () => {
+    getUsers(dispatch);
+  }
+
+  if (isLoading) return <div>로당중...</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return <button onClick={refetch}>불러오기</button>;
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
 
   return (
     <>
@@ -33,7 +48,7 @@ const Users = () => {
           </li>
         ))}
       </ul>
-      <button onClick={refetch}>다시 불러오기</button>
+      <button onClick={fetchData}>다시 불러오기</button>
       {userId && <User id={userId} />}
     </>
   );
